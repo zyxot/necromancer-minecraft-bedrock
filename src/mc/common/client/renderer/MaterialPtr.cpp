375bdd0eb776fb89df42b71ba28cfe8fe91fc6c5
zyxot <102404016+zyxot@users.noreply.github.com>
2026-09-06 20:06:44 -0500

    Fix RenderMaterialGroup material creation on Bedrock 1.26.44+

--- src/mc/common/client/renderer/MaterialPtr.cpp
+++ src/mc/common/client/renderer/MaterialPtr.cpp
@@ -1,6 +1,8 @@
 #include "pch.h"
 #include "MaterialPtr.h"
 #include "util/Util.h"
+#include "client/Necromancer.h"
+#include <charconv>
 
 SDK::MaterialPtr* SDK::MaterialPtr::getUIColor() {
     static auto uiFillColorMaterial = createMaterial(HashedString("ui_fill_color"));
@@ -24,6 +26,23 @@ SDK::MaterialPtr* SDK::MaterialPtr::getSelectionOverlayMaterial() {
 SDK::MaterialPtr* SDK::MaterialPtr::createMaterial(const SDK::HashedString& name) {
     static class RenderMaterialGroup* materialGroup =
         Signatures::RenderMaterialGroup__common.as_ptr<class RenderMaterialGroup>();
-    return memory::callVirtual<SDK::MaterialPtr*, const SDK::HashedString&>(
-        materialGroup, Signatures::VtableIndex::RenderMaterialGroup::createMaterial, name);
+
+    // Bedrock 1.26.44 changed this virtual from returning MaterialPtr* directly
+    // to writing a shared_ptr<MaterialPtr> through an output parameter. Keep the
+    // legacy call for 1.26.40-1.26.43 and use the new ABI for 1.26.44+.
+    int build = 44;
+    const auto& version = Necromancer::get().gameVersion;
+    if (const auto dot = version.rfind('.'); dot != std::string::npos && dot + 1 < version.size()) {
+        std::from_chars(version.data() + dot + 1, version.data() + version.size(), build);
+    }
+
+    if (build < 44) {
+        return memory::callVirtual<SDK::MaterialPtr*, const SDK::HashedString&>(
+            materialGroup, Signatures::VtableIndex::RenderMaterialGroup::createMaterial, name);
+    }
+
+    std::shared_ptr<SDK::MaterialPtr> material {};
+    memory::callVirtual<void, std::shared_ptr<SDK::MaterialPtr>&, const SDK::HashedString&>(
+        materialGroup, Signatures::VtableIndex::RenderMaterialGroup::createMaterial, material, name);
+    return material.get();
 }
